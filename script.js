76ab16db9d63eb1c41140052886a544cfeede051
Will <willbickham3@gmail.com>
2022-12-07 11:33:44 -0500

    Add password validation

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 let psw = document.querySelector("#user-password");
 let confirmPsw = document.querySelector("#confirm-user-password");
+
+psw.onblur = function() {
+    let lowerCaseLetter = /[a-z]/g;
+    let upperCaseLetter = /[A-Z]/g;
+    let num = /[0-9]/g;
+    if(psw.value.match(lowerCaseLetter && upperCaseLetter && num) && psw.value.length >= 8) {
+        psw.classList.remove("invalid");
+        psw.classList.add("valid");
+}   else {
+        psw.classList.remove("valid");
+        psw.classList.add("invalid");
+}
+}
+
+confirmPsw.onblur = function() {
+    if(confirmPsw.value.match(psw.value)) {
+        confirmPsw.classList.remove("invalid");
+        confirmPsw.classList.add("valid");
+}   else {
+        confirmPsw.classList.remove("valid")
+        confirmPsw.classList.add("invalid")
+
+}
+}
